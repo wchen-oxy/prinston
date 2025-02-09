ee57487572ba14ec027d7a3beeaf7718a38ea9ab
unknown <williamshengchen8@gmail.com>
2025-02-09 00:09:07 -0500

    feat: change urls
    - all urls are now prepended with prinston

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   site: "https://wchen-oxy.github.io",
-  base: "prinston",
   adapter: cloudflare(),
 });
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // const isDev = import.meta.DEV;
 // const isProd = import.meta.PROD;
-// const BASE = isDev ? "/prinston/" : import.meta.BASE;
+// const BASE = isDev ? "/" : import.meta.BASE;
 // export const BASE;
 
 const paddingLeft = '6.5rem';
